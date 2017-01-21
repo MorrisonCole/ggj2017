372d19747004c52515ee759837a95cf9688cf6e8
Morrison Cole <morrison@improbable.io>
2017-01-21 15:46:20 +0000

    Fixed ship

--- Assets/RotationClamp.cs
+++ Assets/RotationClamp.cs
@@ -2,12 +2,27 @@
 
 public class RotationClamp : MonoBehaviour
 {
-    public int MinRotation;
-    public int MaxRotation;
+    public int MinimumX;
+    public int MaximumX;
 
     private void Update()
     {
-        var rotation = transform.rotation.eulerAngles;
-        transform.rotation = Quaternion.Euler(Mathf.Clamp(rotation.x, MinRotation, MaxRotation), rotation.y, rotation.z);
+        transform.localRotation = ClampRotationAroundXAxis(transform.localRotation);
+    }
+
+    private Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+
+        angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
+
+        q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
     }
 }
