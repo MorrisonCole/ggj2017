21b03c3e5695b5467d986f0920ad73b75c11663a
Morrison Cole <morrison@improbable.io>
2017-01-21 14:04:27 +0000

    Added Yarn

---
+++ Assets/RotationClamp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotationClamp : MonoBehaviour
+{
+    public int MinRotation;
+    public int MaxRotation;
+
+    private void Update()
+    {
+        var rotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(Mathf.Clamp(rotation.x, MinRotation, MaxRotation), rotation.y, rotation.z);
+    }
+}
---
+++ Assets/ShipControls.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+    public float Speed = 1;
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        transform.position = transform.position + Vector3.right * (Time.deltaTime * horizontal * Speed);
+    }
+}
